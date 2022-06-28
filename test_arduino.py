80d1df0448edd24d36fe7a5bdc104acd595bd61f
python4embeded <python4embeded@gmail.com>
2022-06-28 15:49:12 +0800

    update test_arduino.py

--- test_arduino.py
+++ test_arduino.py
@@ -11,41 +11,38 @@ LOW = 0
 
 if __name__ == '__main__':
 
-    #创建蓝牙连接
-    bl_com = uno.Wireless_Setup(target_name, target_address)
-    ############
-    #不需要设置pinMode函数，系统会自动设置输入输出模式。
+    #初始化arduino uno板
+    arduino = uno.ArduinoUNO(target_name, target_address)
     ############
     #数字端口D5输出低电平
-    #uno.digitalWrite(5, LOW, bl_com)
+    arduino.digitalWrite(5, LOW)
     #数字端口D2输出高电平
-    uno.digitalWrite(2, HIGH, bl_com)
+    arduino.digitalWrite(2, HIGH)
     
     #读取数字端口D9的数值
     pin  = 2
-    a = uno.digitalRead(pin, bl_com)
+    a = arduino.digitalRead(pin)
     print("digital read: ", a)
     time.sleep(0.2)
 
     #数字端口D3输出PWM，占空比为：50/256
-    uno.analogWrite(3, 100, bl_com)
+    arduino.analogWrite(3, 100)
 
-    myservo = uno.Servo(bl_com)
-    myservo.attach(10)
-    print("mysero attach ok")
+    arduino.servo_attach(10)
+    print("sero attach ok")
     time.sleep(2)
-    myservo.write(180)
-    print("myservo move")
+    arduino.servo_write(180)
+    print("servo move")
     #time.sleep(5)
-    myservo.detach()
+    arduino.servo_detach()
     print("servo bye")
 
     #读取模拟端口A0的电压值
-    #for i in range (100):
-    #    b = uno.analogRead(5, bl_com)
-    #    print("analog read: ", b)
-    #    time.sleep(0.2)
+    for i in range (100):
+        b = arduino.analogRead(5)
+        print("analog read: ", b)
+        time.sleep(0.2)
 
     #关闭蓝牙连接
-    uno.Wireless_Close(bl_com)
+    arduino.comm_close()
     
